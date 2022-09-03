f48a3fe0c53e06824c9779cac14592546c95d931
su-su-su-su <suzukikoji0503@gmail.com>
2022-09-03 11:43:26 +0900

    eliminate methodification
    メソッド化を解消

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,28 +1,6 @@
 #!/usr/bin/env ruby
 require "date"
-require 'optparse'
-
-  def firstweek(day_first, day_end)
-    day_first.upto(day_end) {|d|
-      if d.mday == 1 && d.saturday?
-        puts "#{' '.rjust(3) * d.wday}#{d.day.to_s.rjust(3)}"
-      elsif d.mday == 1
-        print "#{' '.rjust(3) * d.wday}#{d.day.to_s.rjust(3)}"
-      end
-    }
-  end
-  def second_week_onwards(day_first, day_end)
-    day_first.upto(day_end) {|d|
-      if d.day == 1
-        next
-      end
-      print  d.day.to_s.rjust(3)
-      if  d.saturday? 
-        puts 
-      end
-    }
-  end
-
+require "optparse"
 now = Date.today
 options = ARGV.getopts("y:", "m:")
 yaer = options["y"] ? options["y"].to_i : now.year
@@ -32,5 +10,11 @@ day_end = Date.new( yaer, month,  -1)
 print "  " * 3 + day_first.month.to_s + "月 "
 puts day_first.year
 puts " 日 月 火 水 木 金 土 "
-firstweek(day_first, day_end)
-second_week_onwards(day_first, day_end)
+print "#{' '.rjust(3) * day_first.wday}"
+
+day_first.upto(day_end) do |d|
+  print d.day.to_s.rjust(3)
+  if d.saturday?
+    puts 
+  end
+end
