3af4ebc8a3c8ca7004ec3c4fe3e5b1d85d123c7c
su-su-su-su <suzukikoji0503@gmail.com>
2022-11-08 12:57:12 +0900

    refactoring
    リファクタリング

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -10,18 +10,15 @@ end
 
 def insert_blank
   directory_max_legth = directory.map(&:length).max # ファイルの最大の文字数を出す
-  directory.each do |name|
-    if name.length < directory_max_legth
-      difference = directory_max_legth - name.length
-      name << (' ' * difference)
-    end
+  directory.map do |file|
+    file.ljust(directory_max_legth + 10)
   end
 end
 
 def sort_by
   name = insert_blank.each_slice(row_legth).to_a
   name[0].zip(*(name[1..nil])) do |i|
-    print i.join(''.ljust(10))
+    print i.join
     puts
   end
 end
