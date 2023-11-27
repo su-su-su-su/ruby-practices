693734f7c26f8d246e225d08c08b048a77c008f2
su-su-su-su <suzukikoji0503@gmail.com>
2023-11-27 15:09:38 +0900

    GameからFrame経由でShotを扱うようにリファクタリング

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -21,4 +21,18 @@ class Frame
   def score
     [first_shot.point, second_shot.point].sum
   end
+
+  def calculate_strike_score(next_frame, next_next_frame)
+    if strike? && next_frame.first_shot.point == 10
+      (next_frame.score + next_next_frame.first_shot.point + score)
+    elsif strike?
+      (next_frame.score + score)
+    end
+  end
+
+  def calculate_spare_score(next_frame)
+    return unless spear?
+
+    (next_frame.first_shot.point + score)
+  end
 end
--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -28,33 +28,20 @@ class Game
   def print_scores
     point = 0
     group_frames.each_with_index do |frame, i|
+      next_frame = group_frames[i + 1]
+      next_next_frame = group_frames[i + 2]
+
       point += if i >= 9 && frame.strike?
                  10
                elsif frame.strike?
-                 calculate_strike_score(frame, i)
+                 frame.calculate_strike_score(next_frame, next_next_frame)
                elsif frame.spear?
-                 calculate_spare_score(frame, i)
+                 frame.calculate_spare_score(next_frame)
                else
                  frame.score
                end
     end
     p point
-  end
-
-  def calculate_strike_score(frame, index)
-    if frame.strike? && group_frames[index + 1].first_shot.point == 10 # 2回連続でストライクを取った場合
-      (group_frames[index + 1].score + group_frames[index + 2].first_shot.point + frame.score)
-    elsif frame.strike?
-      (group_frames[index + 1].score + frame.score)
-    end
-  end
-
-  def calculate_spare_score(frame, index)
-    if index >= 9 && frame.spear?
-      (group_frames[9].score + @shots.last.to_i) # 10フレーム目にスペアを取った場合
-    else
-      (group_frames[index + 1].first_shot.point + frame.score)
-    end
   end
 end
 
