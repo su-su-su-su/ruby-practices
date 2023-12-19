bbdd87d680683be28739106339fbc316ec2257ee
su-su-su-su <suzukikoji0503@gmail.com>
2023-12-19 12:42:55 +0900

    group_framesメソッドをシンプルになるように修正

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -10,43 +10,27 @@ class Game
   def group_frames(shots)
     frame_shots = []
     frames = []
-    shots.each do |s|
-      if s != 'X'
-        s = s.to_i
-        frame_shots << s
-        handle_shot(frame_shots, frames)
+    shots.each_with_index do |shot, index|
+      frame_shots << shot
+      if frames.size == 9
 
-      elsif frames.size == 9 && frame_shots.size <= 2
-        frame_shots << s
-        if frame_shots.size == 3
-          frame_shots << s
-          frames << Frame.new(frame_shots)
-        end
-      else
-        frames << Frame.new([s])
+        handle_final_frame(frame_shots, frames, index, shots)
+        next
       end
+      if shot != 'X'
+        next if frame_shots.size == 1 && frame_shots[0].to_i <= 9
+
+        frames << Frame.new(frame_shots)
+      else
+        frames << Frame.new([shot])
+      end
+      frame_shots = []
     end
     frames
   end
 
-  def handle_shot(shots, frames)
-    if frames.size == 9 && shots.size == 2
-      handle_final_frame(shots, frames)
-    elsif shots.size == 1 && shots[0] <= 9
-      nil
-    else
-      frames << Frame.new(shots)
-      shots.clear
-    end
-  end
-
-  def handle_final_frame(shots, frames)
-    if shots[0].to_i + shots[1].to_i == 10 || shots[0] == 'X'
-      nil
-    else
-      frames << Frame.new(shots)
-      shots.clear
-    end
+  def handle_final_frame(frame_shots, frames, index, shots)
+    frames << Frame.new(frame_shots) if index == shots.size - 1
   end
 
   def print_scores
