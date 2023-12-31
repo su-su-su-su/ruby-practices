86d1efedcdd62c249c8884ab1f8bd49bca964729
su-su-su-su <suzukikoji0503@gmail.com>
2023-12-31 16:26:32 +0900

    private メソッドを使用、group_framesメソッドの重複を修正

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -7,47 +7,43 @@ class Game
     @frames = group_frames(shots)
   end
 
-  def group_frames(shots)
-    frame_shots = []
-    frames = []
-    shots.each_with_index do |shot, index|
-      frame_shots << shot
-      if frames.size == 9
-
-        handle_final_frame(frame_shots, frames, index, shots)
-        next
-      end
-      if shot != 'X'
-        next if frame_shots.size == 1 && frame_shots[0].to_i <= 9
-
-        frames << Frame.new(frame_shots)
-      else
-        frames << Frame.new([shot])
-      end
-      frame_shots = []
-    end
-    frames
-  end
-
-  def handle_final_frame(frame_shots, frames, index, shots)
-    frames << Frame.new(frame_shots) if index == shots.size - 1
-  end
-
   def print_scores
     point = 0
     @frames.each_with_index do |frame, index|
       next_frame = @frames[index + 1]
       next_next_frame = @frames[index + 2]
-
-      point += if frame.strike?
+      point += if frame.strike? && index <= 8
                  frame.calculate_strike_score(index, next_frame, next_next_frame)
-               elsif frame.spare?
+               elsif frame.spare? && index <= 8
                  frame.calculate_spare_score(index, next_frame)
                else
                  frame.score
                end
     end
     p point
+  end
+
+  private
+
+  def group_frames(shots)
+    frame_shots = []
+    frames = []
+    shots.each_with_index do |shot, index|
+      frame_shots << shot
+      if frames.size == 9
+        handle_final_frame(frame_shots, frames, index, shots)
+        next
+      end
+      if shot == 'X' || frame_shots.size == 2
+        frames << Frame.new(frame_shots)
+        frame_shots = []
+      end
+    end
+    frames
+  end
+
+  def handle_final_frame(frame_shots, frames, index, shots)
+    frames << Frame.new(frame_shots) if index == shots.size - 1
   end
 end
 
